32db433a0b988d44467c112232366f5519bdc635
dalldrit13 <dalldrit13@gmail.com>
2022-05-02 12:07:01 -0400

    Fixed missing depencency issue

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { ThemeProvider } from '@material-ui/core';
-import { createMuiTheme } from '@material-ui/core/styles';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 import '../styles/global.css';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#59CE7A',
-    },
-    secondary: {
-      main: '#1D1E33',
-    }
-  }
-})
-
 export default function Wrapper(props) {
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Alldrit Tech | Make your dream a reality</title>
         <meta name="description" content="Hello World! My name is Devin and I'm a computer engineer, founder of a company, singer song writer and I'm trying to change the world one line of code at a time." />
       </Helmet>
-      <Navbar active={props.active}/>
+      <Navbar />
         {props.children}
       <Footer />
-    </ThemeProvider>
+    </>
   );
 } 
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import Button from '@material-ui/core/Button';
-import Wrapper from '../layouts';
-import '../styles/home.css'
+import React from "react";
+import { Link } from 'gatsby'
+// Functional Libraries
 
-export default function FourOhFour() {
+// Constants
+
+// Components
+import Wrapper from "../layouts";
+
+// Images
+import Galaxy from "../images/stars.jpeg";
+
+// Stylesheets
+import "../styles/home.css";
+
+export default function Index() {
   return (
-    <Wrapper active="index">
-      <Helmet>
-        <meta name="robots" content="noindex" />
-      </Helmet>
+    <Wrapper>
       <div className="home-container">
-        <div style={{ fontSize: 36, paddingTop: 140 }}>Sorry we couldn't find the page you're looking for</div>
-        <Button className="learn-btn" variant="contained" color="primary" href="/">Home</Button>
+        <div
+          className="home-landing"
+          style={{
+            backgroundImage: "url(" + Galaxy + ")",
+            backgroundSize: "cover",
+          }}
+        >
+          <div className="title name">Page Not Found</div>
+          <Link className="subtitle descriptors" to="/">
+            Home
+          </Link>
+        </div>
       </div>
     </Wrapper>
-  );
+  )
 }
